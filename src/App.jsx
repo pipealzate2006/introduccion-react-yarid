ca04d86566cc74eb762e7e0288649a5f6c1b22a3
Felipe Alzate Gil <felipegil444@gmail.com>
2024-10-11 14:43:13 -0500

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={<Saludo idioma="en" />}
+            element={<Saludo idioma="es" react="React"/>}
           />
           <Route
             path="/contenedor"
--- src/components/Saludo.jsx
+++ src/components/Saludo.jsx
@@ -1,11 +1,17 @@
-const Saludo = ({ idioma }) => {
+const Saludo = ({ idioma, react }) => {
   const language =
-    idioma === "en" ? "Hello World" : idioma === "es" ? "Hola Mundo" : "短语";
+    idioma === "en"
+      ? "!Welcome to"
+      : idioma === "es"
+      ? "!Bienvenido a"
+      : "!欢迎来到";
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-blue-950">
       <div className="space-y-5">
-        <h1 className="text-white font-bold text-3xl">{language}</h1>
+        <h1 className="text-white font-bold text-3xl">
+          {language} {react}
+        </h1>
       </div>
     </div>
   );
